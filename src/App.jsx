67246a675cb42a1da9c67716d0de2fcb443a421c
Swathi Angadi <angadiswathi119@gmail.com>
2025-09-22 23:10:24 -0700

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import Navbar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Projects from "./components/Projects.jsx";
-import Footer from "./components/Footer.jsx";
 import Experience from "./components/Experience.jsx";
-import Reordering from "./components/Reordering.tsx";
-import CardGrid from "./components/CardGrid.jsx";
 import Contact from "./components/Contacts.jsx";
-import Resume from "./components/Resume.jsx";
 
 function App() {
   return (
@@ -17,7 +13,6 @@ function App() {
       <Hero />
       <About />
       <Experience/>
-      {/* <Resume/> */}
       <Projects />
       <Contact/>
     </div>
