91e97775b51c4afa6c2e661e6f6bac444bddf38e
Swathi Angadi <angadiswathi119@gmail.com>
2025-09-23 23:26:06 -0700

    changed position for background in resume sec

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -59,6 +59,7 @@ export default function Experience(){
     </div>
     </div>
     <section id="resume" className="relative flex flex-col items-center gap-4">
+        
      <div className="absolute inset-0 bg-black/40" />
        <h1 className="text-3xl text-center uppercase z-10">check out my resume</h1>
        <button className="border z-11 p-2" onClick={openResume}> Grab A Copy </button>
